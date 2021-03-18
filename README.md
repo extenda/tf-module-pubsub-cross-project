105cd7a045d51ed699c87d43ce17e2bca415063b
Aleksandr Skoriy <askoriy@users.noreply.github.com>
2021-03-18 14:32:47 +0200

    feat: Refactor for kafka-connect subscription (#2)
    * feat: Refactor for kafka-connect subscription [WIP]
    * feat: Add pubsub.viewer permissions for kafka-connect to work
    * feat: Refactor and strip down to necessary resources only

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ## Description
 
 This module is intended to create subscriptions in current or external (separately specified) projects.
-The default service account will get `roles/pubsub.publisher` role to an external DLQ topic in order to publish and `roles/pubsub.subscriber` role to external topic in order to subscribe to it. Also, `roles/pubsub.subscriber` role is granted in the current project in case there are topics created using the official Pub/Sub module and you want to subscribe to them.
+
+`pull\_subscriptions` parameter supports the same keys that in original [Pubsub](https://github.com/terraform-google-modules/terraform-google-pubsub/tree/v1.9.0) module.
+Additionally `service` key in `pull\_subscriptions` needs to be provided to configure subscriber permissions in the subscription for service's service account.
 
 ## Providers
 
@@ -14,11 +16,9 @@ The default service account will get `roles/pubsub.publisher` role to an externa
 | Name | Description | Type | Default | Required |
 |------|-------------|------|---------|:--------:|
 | create\_subscriptions | Specify true if you want to create subscriptions | `bool` | `true` | no |
-| dead\_letter\_policy | Specify true if you want to add dead letter policy | `bool` | `false` | no |
 | external\_project\_id | The external project ID to manage the Pub/Sub topics. | `string` | n/a | yes |
 | project\_id | The project ID to manage the Pub/Sub resources. | `string` | n/a | yes |
 | pull\_subscriptions | The list of the pull subscriptions | `list(map(string))` | `[]` | no |
-| push\_subscriptions | The list of the push subscriptions | `list(map(string))` | `[]` | no |
 | subscription\_labels | A map of labels to assign to every Pub/Sub subscription | `map(string)` | `{}` | no |
 
 ## Outputs
--- main.tf
+++ main.tf
@@ -1,189 +1,85 @@
-data "google_project" "project" {
-  project_id = var.project_id
-}
-
 locals {
   default_ack_deadline_seconds = 10
-  pubsub_svc_account_email     = "service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+  pull_subscriptions           = { for i in var.pull_subscriptions : i.name => i }
 }
 
-## Publisher role to external dead letter topic
-resource "google_pubsub_topic_iam_member" "push_topic_binding_dead_letter" {
-  count   = var.create_subscriptions ? length(var.push_subscriptions) : 0
-  project = var.external_project_id
-  topic   = lookup(var.push_subscriptions[count.index], "dead_letter_topic", "")
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${local.pubsub_svc_account_email}"
-}
+resource "google_pubsub_subscription_iam_member" "service_pull_subscription_binding" {
+  for_each = local.pull_subscriptions
 
-resource "google_pubsub_topic_iam_member" "pull_topic_binding_dead_letter" {
-  count   = var.dead_letter_policy ? length(var.pull_subscriptions) : 0
-  project = var.external_project_id
-  topic   = lookup(var.pull_subscriptions[count.index], "dead_letter_topic", "")
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${local.pubsub_svc_account_email}"
-}
-## Subscriber role to external project's topics
-resource "google_pubsub_topic_iam_member" "pull_topic_binding" {
-  count   = var.create_subscriptions ? length(var.pull_subscriptions) : 0
-  project = var.external_project_id
-  topic   = lookup(var.pull_subscriptions[count.index], "external_topic")
-  role    = "roles/pubsub.subscriber"
-  member  = "serviceAccount:${local.pubsub_svc_account_email}"
-}
-
-resource "google_pubsub_topic_iam_member" "push_topic_binding" {
-  count   = var.create_subscriptions ? length(var.push_subscriptions) : 0
-  project = var.external_project_id
-  topic   = lookup(var.push_subscriptions[count.index], "external_topic")
-  role    = "roles/pubsub.subscriber"
-  member  = "serviceAccount:${local.pubsub_svc_account_email}"
-}
-
-## Subscriber role to project's topics
-resource "google_pubsub_subscription_iam_member" "pull_subscription_binding" {
-  count        = var.create_subscriptions ? length(var.pull_subscriptions) : 0
-  project      = var.project_id
-  subscription = var.pull_subscriptions[count.index].name
+  project      = var.external_project_id
+  subscription = each.value.name
   role         = "roles/pubsub.subscriber"
-  member       = "serviceAccount:${local.pubsub_svc_account_email}"
+  member       = "serviceAccount:${each.value.service}@${var.project_id}.iam.gserviceaccount.com"
   depends_on = [
     google_pubsub_subscription.pull_subscriptions,
   ]
 }
 
-resource "google_pubsub_subscription_iam_member" "push_subscription_binding" {
-  count        = var.create_subscriptions ? length(var.push_subscriptions) : 0
-  project      = var.project_id
-  subscription = var.push_subscriptions[count.index].name
-  role         = "roles/pubsub.subscriber"
-  member       = "serviceAccount:${local.pubsub_svc_account_email}"
+resource "google_pubsub_subscription_iam_member" "service_pull_subscription_binding_viewer" {
+  for_each = local.pull_subscriptions
+
+  project      = var.external_project_id
+  subscription = each.value.name
+  role         = "roles/pubsub.viewer"
+  member       = "serviceAccount:${each.value.service}@${var.project_id}.iam.gserviceaccount.com"
   depends_on = [
-    google_pubsub_subscription.push_subscriptions,
+    google_pubsub_subscription.pull_subscriptions,
   ]
 }
 
-
-resource "google_pubsub_subscription" "push_subscriptions" {
-  count   = var.create_subscriptions ? length(var.push_subscriptions) : 0
-  name    = var.push_subscriptions[count.index].name
-  topic   = var.push_subscriptions[count.index].external_topic
-  project = var.project_id
-  labels  = var.subscription_labels
-  ack_deadline_seconds = lookup(
-    var.push_subscriptions[count.index],
-    "ack_deadline_seconds",
-    local.default_ack_deadline_seconds,
-  )
-  message_retention_duration = lookup(
-    var.push_subscriptions[count.index],
-    "message_retention_duration",
-    null,
-  )
-  retain_acked_messages = lookup(
-    var.push_subscriptions[count.index],
-    "retain_acked_messages",
-    null,
-  )
-  filter = lookup(
-    var.push_subscriptions[count.index],
-    "filter",
-    null,
-  )
-  enable_message_ordering = lookup(
-    var.push_subscriptions[count.index],
-    "enable_message_ordering",
-    null,
-  )
-  dynamic "expiration_policy" {
-    // check if the 'expiration_policy' key exists, if yes, return a list containing it.
-    for_each = contains(keys(var.push_subscriptions[count.index]), "expiration_policy") ? [var.push_subscriptions[count.index].expiration_policy] : []
-    content {
-      ttl = expiration_policy.value
-    }
-  }
-
-  dynamic "dead_letter_policy" {
-    for_each = (lookup(var.push_subscriptions[count.index], "dead_letter_topic", "") != "") ? [var.push_subscriptions[count.index].dead_letter_topic] : []
-    content {
-      dead_letter_topic     = lookup(var.push_subscriptions[count.index], "dead_letter_topic", "")
-      max_delivery_attempts = lookup(var.push_subscriptions[count.index], "max_delivery_attempts", "5")
-    }
-  }
-
-  dynamic "retry_policy" {
-    for_each = (lookup(var.push_subscriptions[count.index], "maximum_backoff", "") != "") ? [var.push_subscriptions[count.index].maximum_backoff] : []
-    content {
-      maximum_backoff = lookup(var.push_subscriptions[count.index], "maximum_backoff", "")
-      minimum_backoff = lookup(var.push_subscriptions[count.index], "minimum_backoff", "")
-    }
-  }
-
-  push_config {
-    push_endpoint = var.push_subscriptions[count.index]["push_endpoint"]
-
-    dynamic "oidc_token" {
-      for_each = (lookup(var.push_subscriptions[count.index], "oidc_service_account_email", "") != "") ? [true] : []
-      content {
-        service_account_email = lookup(var.push_subscriptions[count.index], "oidc_service_account_email", "")
-        audience              = lookup(var.push_subscriptions[count.index], "audience", "")
-      }
-    }
-  }
-}
-
 resource "google_pubsub_subscription" "pull_subscriptions" {
-  count   = var.create_subscriptions ? length(var.pull_subscriptions) : 0
-  name    = var.pull_subscriptions[count.index].name
-  topic   = var.pull_subscriptions[count.index].external_topic
-  project = var.project_id
+  for_each = local.pull_subscriptions
+
+  name    = each.key
+  topic   = each.value.external_topic
+  project = var.external_project_id
   labels  = var.subscription_labels
   ack_deadline_seconds = lookup(
-    var.pull_subscriptions[count.index],
+    each.value,
     "ack_deadline_seconds",
     local.default_ack_deadline_seconds,
   )
   message_retention_duration = lookup(
-    var.pull_subscriptions[count.index],
+    each.value,
     "message_retention_duration",
     null,
   )
   retain_acked_messages = lookup(
-    var.pull_subscriptions[count.index],
+    each.value,
     "retain_acked_messages",
     null,
   )
   filter = lookup(
-    var.pull_subscriptions[count.index],
+    each.value,
     "filter",
     null,
   )
   enable_message_ordering = lookup(
-    var.pull_subscriptions[count.index],
+    each.value,
     "enable_message_ordering",
     null,
   )
   dynamic "expiration_policy" {
     // check if the 'expiration_policy' key exists, if yes, return a list containing it.
-    for_each = contains(keys(var.pull_subscriptions[count.index]), "expiration_policy") ? [var.pull_subscriptions[count.index].expiration_policy] : []
+    for_each = contains(keys(each.value), "expiration_policy") ? [each.value.expiration_policy] : []
     content {
       ttl = expiration_policy.value
     }
   }
 
   dynamic "dead_letter_policy" {
-    for_each = (lookup(var.pull_subscriptions[count.index], "dead_letter_topic", "") != "") ? [var.pull_subscriptions[count.index].dead_letter_topic] : []
+    for_each = (lookup(each.value, "dead_letter_topic", "") != "") ? [each.value.dead_letter_topic] : []
     content {
-      dead_letter_topic     = lookup(var.pull_subscriptions[count.index], "dead_letter_topic", "")
-      max_delivery_attempts = lookup(var.pull_subscriptions[count.index], "max_delivery_attempts", "5")
+      dead_letter_topic     = lookup(each.value, "dead_letter_topic", "")
+      max_delivery_attempts = lookup(each.value, "max_delivery_attempts", "5")
     }
   }
 
   dynamic "retry_policy" {
-    for_each = (lookup(var.pull_subscriptions[count.index], "maximum_backoff", "") != "") ? [var.pull_subscriptions[count.index].maximum_backoff] : []
+    for_each = (lookup(each.value, "maximum_backoff", "") != "") ? [each.value.maximum_backoff] : []
     content {
-      maximum_backoff = lookup(var.pull_subscriptions[count.index], "maximum_backoff", "")
-      minimum_backoff = lookup(var.pull_subscriptions[count.index], "minimum_backoff", "")
+      maximum_backoff = lookup(each.value, "maximum_backoff", "")
+      minimum_backoff = lookup(each.value, "minimum_backoff", "")
     }
   }
 }
